c8f479d64bf547705a2db5aca69f5865694ca118
Michael Whitten <michael.whitten@ni.com>
2021-04-19 11:39:04 -0500

    ﻿Continued addition of symmetry matrix support for n-port de/embedding

---
+++ Source/MicrowaveNetworks/Matrices/SymmetryExtension/ElementAbstraction.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using MathNet.Numerics.LinearAlgebra.Complex;
+
+namespace MicrowaveNetworks.Matrices.SymmetryExtension
+{
+    public struct ElementAbstraction<T>
+    {
+        T element;
+        public ElementAbstraction(T value) { element = value; }
+        public T Element => element;
+        public ElementAbstraction<T> Add(ElementAbstraction<T> other)
+        {
+            switch (element)
+            {
+                case NetworkParameter c when other.element is NetworkParameter o:
+                    return new ElementAbstraction<T>((T)(object)(c + o));
+                case DenseMatrix a when other.element is DenseMatrix b:
+                    return new ElementAbstraction<T>((T)(object)(a + b));
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+        public ElementAbstraction<T> Subtract(ElementAbstraction<T> other)
+        {
+            switch (element)
+            {
+                case NetworkParameter c when other.element is NetworkParameter o:
+                    return new ElementAbstraction<T>((T)(object)(c - o));
+                case DenseMatrix a when other.element is DenseMatrix b:
+                    return new ElementAbstraction<T>((T)(object)(a - b));
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+        public ElementAbstraction<T> Multiply(ElementAbstraction<T> other)
+        {
+            switch (element)
+            {
+                case NetworkParameter c when other.element is NetworkParameter o:
+                    return new ElementAbstraction<T>((T)(object)(c * o));
+                case DenseMatrix a when other.element is DenseMatrix b:
+                    return new ElementAbstraction<T>((T)(object)(a * b));
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+        public ElementAbstraction<T> Negate()
+        {
+            switch (element)
+            {
+                case NetworkParameter c:
+                    return new ElementAbstraction<T>((T)(object)-c);
+                case DenseMatrix a:
+                    return new ElementAbstraction<T>((T)(object)-a);
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+        public ElementAbstraction<T> Inverse()
+        {
+            switch (element)
+            {
+                case NetworkParameter c:
+                    return new ElementAbstraction<T>((T)(object)(NetworkParameter)Complex.Reciprocal(c));
+                case DenseMatrix a:
+                    return new ElementAbstraction<T>((T)(object)a.Inverse());
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+
+        public static implicit operator T(ElementAbstraction<T> element) => element;
+        public static implicit operator ElementAbstraction<T>(T element) => new ElementAbstraction<T>(element);
+
+        public static ElementAbstraction<T> operator +(ElementAbstraction<T> first, ElementAbstraction<T> second) => first.Add(second);
+        public static ElementAbstraction<T> operator -(ElementAbstraction<T> first, ElementAbstraction<T> second) => first.Subtract(second);
+        public static ElementAbstraction<T> operator -(ElementAbstraction<T> first) => first.Negate();
+        public static ElementAbstraction<T> operator *(ElementAbstraction<T> first, ElementAbstraction<T> second) => first.Multiply(second);
+
+    }
+}
--- Source/MicrowaveNetworks/Matrices/SymmetryExtension/SymmetryMatrix.cs
+++ Source/MicrowaveNetworks/Matrices/SymmetryExtension/SymmetryMatrix.cs
@@ -3,19 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MathNet.Numerics.LinearAlgebra.Complex;
+using System.Numerics;
 
 namespace MicrowaveNetworks.Matrices.SymmetryExtension
 {
-    internal class SymmetryMatrix
+    internal struct SymmetryMatrix<T>
     {
 
-        public SymmetryMatrix(int numPorts) => NumberOfPorts = numPorts;
+        //public SymmetryMatrix(int numPorts) => NumberOfPorts = numPorts;
 
         public int NumberOfPorts { get; set; }
 
-        public SymmetryMatrixElement OneOne { get; set; }
-        public SymmetryMatrixElement OneTwo { get; set; }
-        public SymmetryMatrixElement TwoOne { get; set; }
-        public SymmetryMatrixElement TwoTwo { get; set; }
+        public ElementAbstraction<T> OneOne { get; set; }
+        public ElementAbstraction<T> OneTwo { get; set; }
+        public ElementAbstraction<T> TwoOne { get; set; }
+        public ElementAbstraction<T> TwoTwo { get; set; }
+
+        public ElementAbstraction<T> this[int row, int column]
+        {
+            get
+            {
+                switch (row)
+                {
+                    case 1 when column == 1: return OneOne;
+                    case 1 when column == 2: return OneTwo;
+                    case 2 when column == 1: return TwoOne;
+                    case 2 when column == 2: return TwoTwo;
+                    default: throw new IndexOutOfRangeException();
+                }
+            }
+            set
+            {
+                switch (row)
+                {
+                    case 1 when column == 1: 
+                        OneOne = value;
+                        break;
+                    case 1 when column == 2:
+                        OneTwo = value;
+                        break;
+                    case 2 when column == 1:
+                        TwoOne = value;
+                        break;
+                    case 2 when column == 2:
+                        TwoTwo = value;
+                        break;
+                    default: throw new IndexOutOfRangeException();
+                }
+            }
+        }
+
+
+        public static SymmetryMatrix<NetworkParameter> From2PortData(NetworkParametersMatrix m)
+        {
+            return new SymmetryMatrix<NetworkParameter>
+            {
+                NumberOfPorts = 2,
+
+                OneOne = m[1, 1],
+                OneTwo = m[1, 2],
+                TwoOne = m[2, 1],
+                TwoTwo = m[2, 2]
+            };
+        }
     }
 }
